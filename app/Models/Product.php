f4ec2ba6599817001b52ee3b46518eb53a801ffe
Mahmoud Ahmed Ussien <32422962+Mahmoud126@users.noreply.github.com>
2018-07-28 21:52:28 +0200

    seeding part for product and review

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -1,10 +1,16 @@
 <?php
 
 namespace App\Models;
-
+//use App\Models\Review;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = [
+        'name','details','price','stock','discount',
+    ];
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
 }
